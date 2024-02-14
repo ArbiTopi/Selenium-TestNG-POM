c89cf24f54cfde4faaa7597830addd159c9a0e3d
Arbi Topi <arbi.topi1@gmail.com>
2024-02-14 13:57:11 +0100

    added Extendreports and ExtentTest  to testlisteners and made a method for screenshot on failure

--- src/main/java/utilities/BaseInformation.java
+++ src/main/java/utilities/BaseInformation.java
@@ -1,9 +1,14 @@
 package utilities;
 
 import globals.Globals;
+import org.codehaus.plexus.util.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -37,6 +42,13 @@ public class BaseInformation {
         }
 
         return driver;
+    }
+    public String getScreenShot(String testCaseName) throws IOException{
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        File file = new File(System.getProperty("user.dir")+"//reports//"+testCaseName+".png");
+        FileUtils.copyFile(source,file);
+        return System.getProperty("user.dir")+"//reports//"+ testCaseName +".png";
     }
 
 }
--- src/test/java/listeners/Listeners.java
+++ src/test/java/listeners/Listeners.java
@@ -1,13 +1,24 @@
 package listeners;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import utilities.BaseInformation;
+import utilities.ExtentReporterNG;
 
-public class Listeners implements ITestListener {
+import java.io.IOException;
+
+public class Listeners extends BaseInformation implements ITestListener {
+
+    ExtentTest test;
+    ExtentReports extent = ExtentReporterNG.getReportObject();
     @Override
     public void onFinish(ITestContext Result)
     {
+        extent.flush();
 
     }
 
@@ -27,6 +38,16 @@ public class Listeners implements ITestListener {
     @Override
     public void onTestFailure(ITestResult Result)
     {
+        test.fail(Result.getThrowable());
+        String filepath = null;
+        try {
+            filepath = getScreenShot(Result.getMethod().getMethodName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        test.addScreenCaptureFromPath(filepath,Result.getMethod().getMethodName());
+
+
         System.out.println("The name of the testcase failed is :"+Result.getName());
     }
 
@@ -34,6 +55,7 @@ public class Listeners implements ITestListener {
     @Override
     public void onTestSkipped(ITestResult Result)
     {
+        test.log(Status.SKIP,"Test skipped");
         System.out.println("The name of the testcase Skipped is :"+Result.getName());
     }
 
@@ -41,6 +63,8 @@ public class Listeners implements ITestListener {
     @Override
     public void onTestStart(ITestResult Result)
     {
+        test =extent.createTest(Result.getMethod().getMethodName());
+
         System.out.println(Result.getName()+" test case started");
     }
 
@@ -48,6 +72,7 @@ public class Listeners implements ITestListener {
     @Override
     public void onTestSuccess(ITestResult Result)
     {
+        test.log(Status.PASS,"Test passed sucesfully");
         System.out.println("The name of the testcase passed is :"+Result.getName());
     }
 }
